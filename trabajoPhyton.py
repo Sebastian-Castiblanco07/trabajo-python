a4d92b9cc6643dc1d476460f859c2a613d96d392
Sebastian-Castiblanco07 <jcastiblancos@unal.edu.co>
2026-06-06 21:56:38 -0500

    entrega del trabajo

--- trabajoPhyton.py
+++ trabajoPhyton.py
@@ -1,9 +1,9 @@
 import csv
 
 def liderTabla(equipos): #en realidad es organizar por orden de puntos pero el ejercicio pedia que se llamara asi
+    equiposOrdenados = sorted(equipos.items(), key=lambda x: CalcularPuntos(x[1]), reverse=True)
+    return dict(equiposOrdenados)
    
-   
-    return 0
 def CalcularPuntos(equipo):
     return equipo["ganados"]*3 + equipo["empatados"]*1
 
@@ -23,19 +23,22 @@ with open("input/equiposChampions.csv", "r") as archivo:
             }
 for equipo in dictEquipos:
     dictEquipos[equipo]["puntos"] = CalcularPuntos(dictEquipos[equipo])
+    dictEquipos[equipo]["diferencia_goles"] = dictEquipos[equipo]["goles_favor"] - dictEquipos[equipo]["goles_contra"]
 
-print(dictEquipos)
-"""
+dictEquipos = liderTabla(dictEquipos)
+print("\n############## el ganador es: ##############\n")
+print(list(dictEquipos.keys())[0])
+print("\n","P.D: los partidos ganados, empatados y perdidos, al igual que los puntos y la diferencia de goles de cada equipo fueron impresos en orden dentro de 'equiposSalida.csv' dentro de la carpeta output")
+
 with open("output/equiposSalida.csv", "w", newline="") as archivo:
     escritor = csv.writer(archivo)
 
     escritor.writerow(
-        ["nombre", "ganados", "empatados", "perdidos"]
+        ["nombre", "ganados", "empatados", "perdidos","goles_favor","goles_contra","puntos","diferencia_goles"]
     )
 
     for key, value in dictEquipos.items():
-        print(key, value)
         escritor.writerow(
-            [key, value["ganados"], value["empatados"], value["perdidos"]]
+            [key, value["ganados"], value["empatados"], value["perdidos"], value["goles_favor"],value["goles_contra"],value["puntos"],value["diferencia_goles"]]
         )
-        """
+        
